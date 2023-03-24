d53d5f88cb2dc36a0d974f1895beac0f30c89f07
VectorGits <davidoluremi0@gmail.com>
2023-03-24 19:59:12 +0100

    Adding keyboard nav p1

---
+++ tab.js
@@ -0,0 +1,14 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', (e) => {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+    
+    // Change the tab index of the current tab to -1
+
+    // If the right key is pushed, move to the next tab on the right
+
+    // If the left key is pushed, move to the next tab on the left
+    
+})
