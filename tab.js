8d7ea5473152fc7082e29feb1140876898377213
VectorGits <davidoluremi0@gmail.com>
2023-03-25 13:25:13 +0100

    Fixed keyboard nav

--- tab.js
+++ tab.js
@@ -1,9 +1,12 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-let tabFocus = 0;
 
-tabList.addEventListener('keydown', (e) => {
+tabList.addEventListener('keydown', changeTabFocus);
+
+
+let tabFocus = 0;
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
     
@@ -11,17 +14,24 @@ tabList.addEventListener('keydown', (e) => {
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
        tabs[tabFocus].setAttribute('tabindex', -1);
     }
-
-    // If the right key is pushed, move to the next tab on the right
+    
+    // If the right key is pushed, move to the next tab on the right, if it exists if not, move to the first tab
     if (e.keyCode === keydownRight) {
         tabFocus += 1;
-        console.log(tabFocus);
-    }
-
-    // If the left key is pushed, move to the next tab on the left
-    if (e.keyCode === keydownLeft) {
-        tabFocus -= 1;
-        console.log(tabFocus);
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
     }
     
-})
+    // If the left key is pushed, move to the next tab on the left, if it exists if not, move to the last tab
+    if (e.keyCode === keydownLeft) {
+        tabFocus -= 1;
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
+    }
+    
+    tabs[tabFocus].setAttribute("tabIndex", 0);
+    tabs[tabFocus].focus();
+    
+}
