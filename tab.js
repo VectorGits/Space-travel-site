3bc57b90ee30db5b5be4582129228223d7c6a6bc
VectorGits <davidoluremi0@gmail.com>
2023-03-29 02:39:28 +0100

    Refactored js code

--- tab.js
+++ tab.js
@@ -13,31 +13,28 @@ let tabFocus = 0;
 function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
-    
+
     // Change the tab index of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-       tabs[tabFocus].setAttribute('tabindex', -1);
+        tabs[tabFocus].setAttribute('tabindex', -1);
+            // If the right key is pushed, move to the next tab on the right, if it exists if not, move to the first tab
+            if (e.keyCode === keydownRight) {
+                tabFocus += 1;
+                if (tabFocus >= tabs.length) {
+                    tabFocus = 0;
+                }
+            } else if (e.keyCode === keydownLeft) {
+                tabFocus -= 1;
+                if (tabFocus < 0) {
+                    tabFocus = tabs.length - 1;
+                }
+            }
+        
+            tabs[tabFocus].setAttribute("tabIndex", 0);
+            tabs[tabFocus].focus();
     }
-    
-    // If the right key is pushed, move to the next tab on the right, if it exists if not, move to the first tab
-    if (e.keyCode === keydownRight) {
-        tabFocus += 1;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
-        }
-    }
-    
-    // If the left key is pushed, move to the next tab on the left, if it exists if not, move to the last tab
-    if (e.keyCode === keydownLeft) {
-        tabFocus -= 1;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-    
-    tabs[tabFocus].setAttribute("tabIndex", 0);
-    tabs[tabFocus].focus();
-    
+
+
 }
 
 
@@ -46,36 +43,41 @@ function changeTabPanel(e) {
     const targetPanel = targetTab.getAttribute('aria-controls');
     const targetImage = targetTab.getAttribute('data-image');
 
-    
+
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
-    
+
     tabContainer
         .querySelector('[aria-selected="true"]')
         .setAttribute('aria-selected', false);
 
-        targetTab.setAttribute('aria-selected', true);
+    targetTab.setAttribute('aria-selected', true);
 
-        
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute('hidden', true));
-        
-        mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
-        
-        
-        // console.log(mainContainer); // This is the id of the panel that needs to be shown
-        // console.log(targetPanel); // This is the id of the panel that needs to be shown
-        // console.log(targetTab); // This is the tab that was clicked
-        // console.log(tabContainer); // This is the tablist
-        // console.log(mainContainer); // This is the main container
+    
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
 
-        // Hide all the images
-        mainContainer
-        .querySelectorAll('picture')
-            .forEach((pic) => pic.setAttribute('hidden', true));
-            
-            
-            mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-            
-    }
+
+    // console.log(mainContainer); // This is the id of the panel that needs to be shown
+    // console.log(targetPanel); // This is the id of the panel that needs to be shown
+    // console.log(targetTab); // This is the tab that was clicked
+    // console.log(tabContainer); // This is the tablist
+    // console.log(mainContainer); // This is the main container
+
+    // Hide all the images
+
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+
+
+}
+
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute('hidden', true));
+}
+
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
+}
